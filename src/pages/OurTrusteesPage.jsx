3d87f68483107c9b3ea0c9411800eeb22dcb0d37
Het Heet <atmiyastudy12@gmail.com>
2026-09-21 23:25:02 +0530

    removed

--- src/pages/OurTrusteesPage.jsx
+++ src/pages/OurTrusteesPage.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Header from "../components/Header";
 import PremiumTrusteesSection from "../components/PremiumTrusteesSection";
 import Footer from "../components/Footer";
-import SpecialThanks from "../components/SpecialThanks";
+
 const OurTrusteesPage = () => {
   return (
     <>
       <Header />
-      <SpecialThanks />
+   
       <PremiumTrusteesSection />
       <Footer />
     </>
